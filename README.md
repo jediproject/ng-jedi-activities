c725fa698cb244dd81c3dde03746f43428d7c7e1
mateuscg_cit <mateuscg@ciandt.com>
2015-09-03 13:27:53 -0300

    Few adjustments to setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# ng-jedi-download
+Easy way to make download requests.
--- download.js
+++ download.js
@@ -4,7 +4,13 @@ define([], function(){
 
 	angular.module('jedi.download', []);
 
-	angular.module('jedi.download').service('jedi.download.DownloadService', [function(){
+	angular.module('jedi.download').service('jedi.download.DownloadService', ['$http', function($http){
+		this.initDownload = function(baseUrl, apiUrl, params, name){
+			$http.get(baseUrl + apiUrl).success(function(responseData){
 
+			}).error(function(responseData){
+
+			});
+		};
 	}]);
 });
