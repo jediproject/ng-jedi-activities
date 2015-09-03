b86d1e5a298f613339e3b516bc6ca5dfbed20f64
mateuscg_cit <mateuscg@ciandt.com>
2015-09-03 09:53:08 -0300

    Initial Setup
    Added bower.json, README.md and download.js.

---
+++ download.js
@@ -0,0 +1,10 @@
+'use strict';
+
+define([], function(){
+
+	angular.module('jedi.download', []);
+
+	angular.module('jedi.download').service('jedi.download.DownloadService', [function(){
+
+	}]);
+});
