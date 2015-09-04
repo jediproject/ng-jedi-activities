48b343d7e911ce55ff06378d2e04af335af97b71
mateuscg_cit <mateuscg@ciandt.com>
2015-09-04 14:56:54 -0300

    Changes to Service/Directive and Style

--- download.js
+++ download.js
@@ -1,47 +1,62 @@
 'use strict';
 
-define(['angular'], function(){
+define(['angular'], function () {
 
-	angular.module('jedi.download', []);
+    angular.module('jedi.download', []);
 
-	var downloadItems = [];
+    var downloadItems = [];
 
-	angular.module('jedi.download').service('jedi.download.DownloadService', ['$http', function($http){
-		this.initDownload = function(baseUrl, apiUrl, params, name){
+    angular.module('jedi.download').service('jedi.download.DownloadService', ['$http', function ($http) {
+        this.initDownload = function (baseUrl, apiUrl, method, params, name) {
 
-			var downloadItem = {name: name,status: 'progress'}	;
-			downloadItems.push(downloadItem);
+            var downloadItem = { name: name, status: 'progress' };
 
-			$http.get(baseUrl + apiUrl).success(function(responseData){
-				downloadItem.status = 'success';
-			}).error(function(responseData){
-				downloadItem.status = 'error';
-			});
-		};
-	}]).directive('jdDownload', [function(){
+            for (var i = 0; i < 20; i++) {
+                downloadItems.push(downloadItem);
+            }
 
-		return{
-			restrict: 'E',
-			replace: true,
-			link: function(scope, element){
-				element.hide();
-				scope.downloadItems = downloadItems;
-				scope.$watch(function(){
-					return downloadItems.length;
-				},
-				function(value){
-					if (value && value > 0) {
-						element.show();
-					}
+            var request = {
+                method: method.toUpperCase(),
+                url: baseUrl + '/' + apiUrl,
+                data: params
+            }
+
+            $http(request).success(function (responseData) {
+                downloadItem.status = 'success';
+            }).error(function (responseData) {
+                downloadItem.status = 'error';
+            });
+        };
+    }]).directive('jdDownload', [function () {
+
+        return {
+            restrict: 'E',
+            replace: true,
+            link: function (scope, element) {
+                element.hide();
+                scope.$watch(function () {
+                    return downloadItems.length;
+                },
+				function (value) {
+				    if (value && value > 0) {
+				        element.show();
+				    }
 				});
-			},
-			templateUrl: function(elem, attrs){
-				if (attrs.templateUrl) {
-					return attrs.templateUrl;
-				} else {
-					return "assets/libs/ng-jedi-download/download.html";
-				}
-			},
-		}
-	}]);
+
+                scope.$on('$destroy', function () {
+                    element.remove();
+                });
+            },
+            controller: ['$scope', '$attrs', '$element', '$timeout', function Controller($scope, $attrs, $element, $timeout) {
+                $scope.downloadItems = downloadItems;
+            }],
+            templateUrl: function (elem, attrs) {
+                if (attrs.templateUrl) {
+                    return attrs.templateUrl;
+                } else {
+                    return "assets/libs/ng-jedi-download/download.html";
+                }
+            },
+        }
+    }]);
 });
