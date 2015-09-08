bd1286e35f002a2939aa652e68b93c241a3309f1
unknown <henriqueb@cit012232bh.cit>
2015-09-08 18:02:20 -0300

    Add the clearDownloads function to the directive and the service

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function () {
+define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function() {
 
     angular.module('jedi.download', []);
 
@@ -9,15 +9,15 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
     function guid() {
         function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
-                       .toString(16)
-                       .substring(1);
+                .toString(16)
+                .substring(1);
         }
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-          s4() + '-' + s4() + s4() + s4();
+            s4() + '-' + s4() + s4() + s4();
     }
 
-    angular.module('jedi.download').service('jedi.download.DownloadService', ['$http', 'localStorageService', function ($http, localStorageService) {
-        this.initDownload = function (baseUrl, apiUrl, method, params, name) {
+    angular.module('jedi.download').service('jedi.download.DownloadService', ['$http', 'localStorageService', '$rootScope', function($http, localStorageService, $rootScope) {
+        this.initDownload = function(baseUrl, apiUrl, method, params, name) {
 
             var downloadItem = {
                 id: guid(),
@@ -31,45 +31,57 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
                 method: method.toUpperCase(),
                 url: baseUrl + '/' + apiUrl,
                 data: params,
-                config: { responseType: 'arraybuffer', ignoreLoadingBar: true, showLoadingModal: true }
+                config: {
+                    responseType: 'arraybuffer',
+                    ignoreLoadingBar: true,
+                    showLoadingModal: true
+                }
             };
 
-            $http(request).success(function (data, status, headers, config) {
+            $http(request).success(function(data, status, headers, config) {
 
                 var contentDisposition = headers("content-disposition");
                 var filename = contentDisposition.substring((contentDisposition.indexOf('filename=') + 9));
 
                 downloadItem.status = 'success';
                 downloadItem.fileName = filename;
-                downloadItem.data = new Blob([data], { type: headers("content-type") });
+                downloadItem.data = new Blob([data], {
+                    type: headers("content-type")
+                });
                 localStorageService.set('download-' + downloadItem.id, downloadItem)
-            }).error(function (data, status) {
+            }).error(function(data, status) {
                 downloadItem.status = 'error';
                 downloadItem.data = null;
                 localStorageService.set('download-' + downloadItem.id, downloadItem)
             });
         };
-    }]).directive('jdDownload', ['$log', function ($log) {
+
+        this.clearDownloads = function clearDownloads() {
+            $rootScope.$broadcast('jedi.download.ClearDownloads');
+            console.log('cleanDownloads');
+        };
+    }]).directive('jdDownload', ['$log', function($log) {
 
         return {
             restrict: 'E',
             replace: true,
-            link: function (scope, element) {
-                scope.$watch(function () {
-                    return downloadItems.length;
-                },
-                    function (value) {
+            link: function(scope, element, attrs, activitiesCtrl) {
+                scope.$watch(function() {
+                        return downloadItems.length;
+                    },
+                    function(value) {
                         if (value && value > 0) {
                             element.show(300);
-                        }
-                        else {
+                        } else {
                             element.hide(300);
                         }
                     });
 
-                scope.$on('$destroy', function () {
+                scope.$on('$destroy', function() {
                     element.remove();
                 });
+
+                scope.$on('jedi.download.ClearDownloads', activitiesCtrl.clear);
             },
             controller: ['$scope', '$attrs', '$element', '$timeout', '$log', 'localStorageService', function Controller($scope, $attrs, $element, $timeout, $log, localStorageService) {
 
@@ -85,6 +97,7 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
                 vm.refresh = refresh;
                 vm.close = close;
                 vm.hasItemsToShow = hasItemsToShow;
+                vm.clear = clear;
 
                 initCtrl();
 
@@ -99,14 +112,14 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
                     $log.info("Removendo item " + item.name);
                     var index = downloadItems.indexOf(item);
                     downloadItems.splice(index, 1);
-                    localStorageService.remove('download-' + item.id)
+                    localStorageService.remove('download-' + item.id);
                 }
 
                 function saveIconClick(item) {
                     if (item.status != 'success') {
                         return false;
                     }
-                    $log.info("Salvando item " + item.name);
+                    $log.info("Salvando item " + item.fileName);
                     saveAs(item.data, item.fileName);
                 }
 
@@ -116,16 +129,25 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
 
                 function close() {
                     $log.info("Fechando lista de downloads");
-                    downloadItems = [];
-
+                    clear();
                 }
+
                 function hasItemsToShow() {
                     return downloadItems && downloadItems.length > 0;
+                }
+
+                function clear() {
+                    for (var i = downloadItems.length - 1; i >= 0; i--) {
+                        var id = downloadItems[i].id;
+                        downloadItems.splice(i, 1);
+                        localStorageService.remove('download-' + id);
+                    }
+                    $log.info('Lista de Downloads removida');
                 }
             }],
             controllerAs: 'activitiesCtrl',
             bindToController: true,
-            templateUrl: function (elem, attrs) {
+            templateUrl: function(elem, attrs) {
                 if (attrs.templateUrl) {
                     return attrs.templateUrl;
                 } else {
@@ -133,11 +155,11 @@ define(['angular', 'file-saver-saveas-js', 'angular-local-storage'], function ()
                 }
             },
         };
-    }]).run(['localStorageService', function (localStorageService) {
+    }]).run(['localStorageService', function(localStorageService) {
 
         var lsKeys = localStorageService.keys();
 
-        angular.forEach(lsKeys, function (key) {
+        angular.forEach(lsKeys, function(key) {
             if (key.toLowerCase().indexOf('download-') > -1) {
                 downloadItems.push(localStorageService.get(key));
             }
